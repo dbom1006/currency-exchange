34bd179ce9752955ccec2d3af9f34443e6057226
Duc Thai <dbom.1006@gmail.com>
2021-11-06 11:58:11 +0700

    Add source code about exchange currency

---
+++ src/utils/helper.js
@@ -0,0 +1,3 @@
+export const formatNumber = (value)=>{
+    return Math.round(value*1000)/1000;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      width:{
+        '104':'26rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
